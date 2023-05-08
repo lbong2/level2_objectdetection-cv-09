b9d96be47cc8f86eceb38c40acc45c739b1b2417
lbong2 <dkw05121@konkuk.ac.kr>
2023-05-08 23:37:00 +0000

    Merge branch 'fix-17/dist-error' of https://github.com/boostcampaitech5/level2_objectdetection-cv-09 into feat-13/inference

---
+++ inference.py
@@ -0,0 +1,119 @@
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+import numpy as np
+import cv2
+import os
+
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+import torch
+# faster rcnn model이 포함된 library
+import torchvision
+
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+from torch.utils.data import DataLoader, Dataset
+import pandas as pd
+from tqdm import tqdm
+
+class CustomDataset(Dataset):
+    '''
+      data_dir: data가 존재하는 폴더 경로
+      transforms: data transform (resize, crop, Totensor, etc,,,)
+    '''
+
+    def __init__(self, annotation, data_dir):
+        super().__init__()
+        self.data_dir = data_dir
+        # coco annotation 불러오기 (coco API)
+        self.coco = COCO(annotation)
+
+    def __getitem__(self, index: int):
+        
+        image_id = self.coco.getImgIds(imgIds=index)
+
+        image_info = self.coco.loadImgs(image_id)[0]
+        
+        image = cv2.imread(os.path.join(self.data_dir, image_info['file_name']))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        image /= 255.0
+
+        ann_ids = self.coco.getAnnIds(imgIds=image_info['id'])
+        anns = self.coco.loadAnns(ann_ids)
+
+        image = torch.tensor(image, dtype=torch.float32).permute(2,0,1)
+
+        return image
+    
+    def __len__(self) -> int:
+        return len(self.coco.getImgIds())
+    
+def inference_fn(test_data_loader, model, device):
+    outputs = []
+    for images in tqdm(test_data_loader):
+        # gpu 계산을 위해 image.to(device)
+        images = list(image.to(device) for image in images)
+        """
+                    for i in range(num_images):
+                result.append(
+                    {
+                        "boxes": boxes[i],
+                        "labels": labels[i],
+                        "scores": scores[i],
+                    }
+                )
+        """
+        output = model(images)
+        for out in output:
+            outputs.append({'boxes': out['boxes'].tolist(), 'scores': out['scores'].tolist(), 'labels': out['labels'].tolist()})
+    return outputs
+
+def main():
+    annotation = '../../dataset/test.json' # annotation 경로
+    data_dir = '../../dataset' # dataset 경로
+    test_dataset = CustomDataset(annotation, data_dir)
+    score_threshold = 0.05
+    check_point = './checkpoints/faster_rcnn_torchvision_checkpoints.pth' # 체크포인트 경로
+    
+
+    test_data_loader = DataLoader(
+        test_dataset,
+        batch_size=8,
+        shuffle=False,
+        num_workers=4
+    )
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print(device)
+    
+    # torchvision model 불러오기
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    num_classes = 11  # 10 class + background
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    model.to(device)
+    model.load_state_dict(torch.load(check_point))
+    model.eval()
+    
+    outputs = inference_fn(test_data_loader, model, device)
+    prediction_strings = []
+    file_names = []
+    coco = COCO(annotation)
+
+    # submission 파일 생성
+    for i, output in enumerate(outputs):
+        prediction_string = ''
+        image_info = coco.loadImgs(coco.getImgIds(imgIds=i))[0]
+        for box, score, label in zip(output['boxes'], output['scores'], output['labels']):
+            if score > score_threshold: 
+                # label[1~10] -> label[0~9]
+                prediction_string += str(label-1) + ' ' + str(score) + ' ' + str(box[0]) + ' ' + str(
+                    box[1]) + ' ' + str(box[2]) + ' ' + str(box[3]) + ' '
+        prediction_strings.append(prediction_string)
+        file_names.append(image_info['file_name'])
+    submission = pd.DataFrame()
+    submission['PredictionString'] = prediction_strings
+    submission['image_id'] = file_names
+    submission.to_csv('./faster_rcnn_torchvision_submission.csv', index=None)
+    print(submission.head())
